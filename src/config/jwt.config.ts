27b9cafd95bd47f0ca7ef6e6f998f18fe3b56f35
Ali Hussein <ali.hussein@live.com>
2024-12-20 16:59:24 +0200

    Updated JWT strategy

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -5,16 +5,20 @@ import { Blacklist } from '../model/blacklist.model';
 import dotenv from 'dotenv'
 
 dotenv.config()
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
 
 const jwtOptions: StrategyOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: ACCESS_TOKEN_SECRET
 };
 
-
 passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
   try {
+    const user = await User.findById(payload.sub);
+    if (!user) {
+      return done(null, false);
+    }
+
     const token = ExtractJwt.fromAuthHeaderAsBearerToken()(payload);
     if (token) {
       const blacklisted = await Blacklist.findOne({ token });
@@ -23,12 +27,7 @@ passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
       }
     }
 
-    const user = await User.findById(payload.sub);
-    if (user) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
+    return done(null, user);
   } catch (error) {
     return done(error, false);
   }
