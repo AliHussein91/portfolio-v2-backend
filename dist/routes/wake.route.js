05d49fe590dc55b510853ffb0a665a1a74f42489
Ali Hussein <ali.hussein@live.com>
2024-12-23 17:29:30 +0200

    Created server wake route

--- dist/routes/wake.route.js
+++ dist/routes/wake.route.js
@@ -4,6 +4,6 @@ exports.WakeRouter = void 0;
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 router.get('/', (req, res) => {
-    res.send('Awake!');
+    res.status(200).json({ 'message': 'Server is awake!' });
 });
 exports.WakeRouter = router;
