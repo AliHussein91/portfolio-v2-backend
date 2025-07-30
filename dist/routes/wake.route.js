cfff1a5721ae991388a4644301560d9c6ed4f901
Ali Hussein <ali.hussein@live.com>
2025-07-30 14:31:35 +0300

    Delete dist directory

--- dist/routes/wake.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WakeRouter = void 0;
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.status(200).json({ 'message': 'Server is awake!' });
-});
-exports.WakeRouter = router;
